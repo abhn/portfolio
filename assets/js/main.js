31409ee8114216f505fc787c0faea430ac1b672e
Abhishek Nagekar <abhishek@nagekar.com>
2018-08-04 01:54:02 +0530

    .

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(() => {
+    head_name_util()
+})
+
+let head_name_util = () => {
+    const NAME = "Abhishek Nagekar"
+    name_html = ''
+    _.map(NAME).forEach(c => {
+        if(c == ' ') c = '&nbsp;';
+        name_html += `<span class="head-title animated">${c}</span>`
+    })
+    
+    $('#head-name').html(name_html)
+    
+    $('.head-title').hover(function() {
+        $(this).addClass('infinite rubberBand');
+        window.setTimeout(() => {
+            $(this).removeClass('infinite rubberBand')
+        }, 1000)
+    })
+}
+
