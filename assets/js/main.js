5d2d0e5ab2b20b9dcd2f179bc54506a5754b04ad
Abhishek Nagekar <abhishek@nagekar.com>
2018-08-28 02:13:11 +0530

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ let blog_posts = () => {
                 </div>
         
                 <div class="blog-goto-link">
-                    <i class="fa fa-greater-than"></i>
+                    <img src="/assets/images/right-open-mini.svg"/>
                 </div>
             </div>
             `;
@@ -63,7 +63,7 @@ let blog_posts = () => {
             </div>
     
             <div class="blog-goto-link">
-                <i class="fa fa-greater-than"></i>
+                <img src="/assets/images/right-open-mini.svg"/>
             </div>
         </div>
         `;
