3a4fa79460f9bb057cbd539c5d65f4d3e246c48c
Abhishek Nagekar <abhishek@nagekar.com>
2018-08-31 21:02:55 +0530

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ let general_utils = () => {
     // smooth scrolling for nav links
     $('.head-menu-wrap a').smoothScroll();
     $('.extra-link a').smoothScroll();
+    $('.profile-pic-link').smoothScroll();
 
     $('.skillbar').each(function(){
 		$(this).find('.skillbar-bar').animate({
